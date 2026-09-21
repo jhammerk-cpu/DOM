46a1b286c8b9c75fcc37f2f549a916b2c7b4d20c
Joshua Kimbell <kimbell_joshua@students.columbusstate.edu>
2026-09-21 12:21:44 -0400

    Added a button which hides the click counter.

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ btn.addEventListener('click', function() {
     clickCount++;
     document.getElementById('clickCount').textContent = clickCount;
 });
+
+document.getElementById('HideClickCounter').addEventListener('click', () => {
+      document.getElementById('counter').classList.toggle('hidden');
+  });
+
